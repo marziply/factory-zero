be6308c07ff237ad290f425b4d75955dbd0b2a64
Hayden Perry <haydencwperry@gmail.com>
2020-07-12 22:30:57 +0100

    Updated ZeroOptions docs

--- index.mjs
+++ index.mjs
@@ -1,6 +1,19 @@
 import Zero from './src/zero.mjs'
 
 /**
+ * @typedef {object} ZeroOptions - Factory Zero options schema.
+ * @property {string} pk - Default primary key for all models.
+ * @property {string} directory - Path to fixture files.
+ * @property {Array.<string>} extensions - Fixture file extensions to search for.
+ * @property {object} keys - Configurable keys for storing options relating to this program.
+ * @property {string} keys.options - Key to use on Model instances for options.
+ * @property {string} keys.model - Key to use for model options within the fixtures.
+ * @property {object} suffixes - Polymorphic column suffixes.
+ * @property {string} suffixes.type - Suffix used for the polymorphic type column.
+ * @property {string} suffixes.id - Suffix used for the polymorphic id column.
+ */
+
+/**
  * @param {Knex|object} knex - Knex instance or a knex connection object.
  * @param {ZeroOptions} options - Factory Zero options.
  *
