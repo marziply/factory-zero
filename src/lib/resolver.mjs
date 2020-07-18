fd2c59d26646a227abbb2fcc2575b9abbfcfb5e4
Hayden Perry <haydencwperry@gmail.com>
2020-07-18 18:10:39 +0100

    Added JSDocs to lib/Resolver

--- src/lib/resolver.mjs
+++ src/lib/resolver.mjs
@@ -4,13 +4,27 @@ import Table from './table.mjs'
 
 const { entries } = Object
 
+/**
+ * Fixture resolver for resolving table columns and configured relationships.
+ * This supports polymorphism which is configurable via [ZeroOptions].
+ *
+ * @class Resolver
+ */
 export default class Resolver {
+  /**
+   * @param {ZeroOptions} options - Configuration for Factory Zero.
+   */
   constructor (options) {
     this.options = options
     this.insertMap = new Map()
     this.relationMap = new Map()
   }
 
+  /**
+   * Resolves relations and returns a map of all insertable relations.
+   *
+   * @returns {Map.<string,object>} - All fixtures with relations resolved.
+   */
   fixtures () {
     const { relationMap, insertMap } = this
 
@@ -21,6 +35,11 @@ export default class Resolver {
     return this.insertMap
   }
 
+  /**
+   * Fetches a new instance of Table for each fixture file.
+   *
+   * @returns {Array.<Fixture>} - Unresolved instances of Fixture.
+   */
   fixtureTables () {
     return entries(this.options.fixtures)
       .flatMap(([tableName, fixture]) => {
@@ -30,6 +49,13 @@ export default class Resolver {
       })
   }
 
+  /**
+   * Fetches a new instance of Model for each instance of Table.
+   *
+   * @param {Table} table - Table instance to resolve relations against.
+   *
+   * @returns {Array.<Fixture>} - Unresolved instances of Fixture.
+   */
   fixtureModels (table) {
     for (const { data, name } of table.fixtures) {
       const model = new Model(this.options, table, data)
@@ -42,6 +68,13 @@ export default class Resolver {
     return table.fixtures
   }
 
+  /**
+   * Searches the given fixture model for possible relatable columns.
+   *
+   * @param {Model} model - Model instance to search for relations on.
+   *
+   * @returns {object} - Collection of relations to resolve later.
+   */
   relations (model) {
     const relations = filterValues(model, v => v.toString().match(/^@[\w]+/g))
 
@@ -50,6 +83,14 @@ export default class Resolver {
     return relations
   }
 
+  /**
+   * Applies polymorphism to columns that can be polymorphically related.
+   *
+   * @param {Model} model - Model instance to search for polymorphic relations on.
+   * @param {object} relations - Collection of relations to check for polymorphism on.
+   *
+   * @returns {void}
+   */
   applyPolymorphism (model, relations) {
     const {
       table: { columns },
