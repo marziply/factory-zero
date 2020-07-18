95559cb26fb4ad3c683c22bb946ad997e23e506e
Hayden Perry <haydencwperry@gmail.com>
2020-07-18 18:44:55 +0100

    Added JSDocs to lib/Table

--- src/lib/table.mjs
+++ src/lib/table.mjs
@@ -3,7 +3,17 @@ import defaults from 'lodash/defaults.js'
 import upperFirst from 'lodash/upperFirst.js'
 import Fixture from './fixture.mjs'
 
+/**
+ * An instance of an individual database table.
+ *
+ * @class Table
+ */
 export default class Table {
+  /**
+   * @param {ZeroOptions} options - Configuration for Factory Zero.
+   * @param {string} tableName - Name of the table on this instance.
+   * @param {Fixture} fixture - Fixture instance to bind onto this table instance.
+   */
   constructor (options, tableName, { model, data }) {
     this.pk = model.pk ?? options.pk
     this.name = tableName
@@ -12,6 +22,13 @@ export default class Table {
     this.model = this.modelDefaults(model)
   }
 
+  /**
+   * Creates an options object with default values.
+   *
+   * @param {Model} model - Model instance to build the options against.
+   *
+   * @returns {object} - Defaulted options.
+   */
   modelDefaults (model) {
     return defaults(model, {
       name: upperFirst(pluralize.singular(this.name)),
@@ -19,10 +36,20 @@ export default class Table {
     })
   }
 
+  /**
+   * All columns defined on the database table this instance refers to.
+   *
+   * @returns {object} - Database columns.
+   */
   get columns () {
     return this.options.tablesInfo[this.name]
   }
 
+  /**
+   * A collection of Fixture instances relating to this table instance.
+   *
+   * @returns {Array.<Fixture>} - Collection of Fixture instances.
+   */
   get fixtures () {
     return Object.entries(this.data)
       .map(([name, data]) => new Fixture(name, data))
